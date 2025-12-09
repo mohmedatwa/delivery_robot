fefa78f38b07f9c67ea6b00305b68e1ed6284f17
Mohamed Atwa <mohamedatwa50@hotmail.com>
2025-12-09 18:06:17 +0200

    add launch simulation to bringup pkg

---
+++ delivery_bringup/launch/simulated_delivery_robot.launch.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3 
+
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    gazebo = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("delivery_description"),
+            "launch",
+            "gazebo.launch.py"
+        ),
+    )
+    
+    controller = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("delivery_controller"),
+            "launch",
+            "controller.launch.py"
+        )
+    )
+
+    rviz_config = os.path.join(
+    get_package_share_directory("delivery_description"),
+    "rviz",
+    "display.rviz")
+
+    rviz_node = Node(
+    package="rviz2",
+    executable="rviz2",
+    name="rviz2",
+    output="screen",
+    arguments=["-d", rviz_config],
+    parameters=[{"use_sim_time": True}]
+    )     
+        
+    
+    joystick = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("delivery_joystick"),
+            "launch",
+            "joy_teleop.launch.py"
+        ),
+        launch_arguments={
+            "use_sim_time": "True"
+        }.items()
+    )
+    
+    return LaunchDescription([
+        gazebo,
+        controller,
+        rviz_node,
+        joystick,
+    ])
