e6a080b7eb114c14e118bcdb5b43dd5bdca125d3
Mohamed Atwa <mohamedatwa50@hotmail.com>
2025-12-13 17:06:08 +0200

    add gz world args to launch file

--- delivery_description/launch/gazebo.launch.py
+++ delivery_description/launch/gazebo.launch.py
@@ -1,11 +1,11 @@
-#/usr/bin/env python3
 import os
+from os import pathsep
 from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, PythonExpression
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -13,36 +13,22 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
-    
-    
-    
-    
-    
     delivery_description = get_package_share_directory("delivery_description")
 
-    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
-                                        delivery_description, "urdf", "delivery.urdf.xacro"
-                                        ),
-                                      description="Absolute path to robot urdf file"
+    model_arg = DeclareLaunchArgument(
+        name="model", default_value=os.path.join(
+                delivery_description, "urdf", "delivery.urdf.xacro"
+            ),
+        description="Absolute path to robot urdf file"
     )
 
-    gazebo_resource_path = SetEnvironmentVariable(
-        name="GZ_SIM_RESOURCE_PATH",
-        value=[
-            str(Path(delivery_description).parent.resolve())
-            ]
-        )
-    
-    ros_distro = os.environ["ROS_DISTRO"]
-    is_ignition = "True" if ros_distro == "humble" else "False"
-    
-    robot_description = ParameterValue(Command([
-            "xacro ",
-            LaunchConfiguration("model"),
-            " is_ignition:=",
-            is_ignition
-        ]),
-        value_type=str
+    world_name_arg = DeclareLaunchArgument(name="world_name", default_value="empty")
+
+    world_path = PathJoinSubstitution([
+            delivery_description,
+            "worlds",
+            PythonExpression(expression=["'", LaunchConfiguration("world_name"), "'", " + '.world'"])
+        ]
     )
 
     bridge_config = os.path.join(
@@ -51,7 +37,25 @@ def generate_launch_description():
     "ros_gz_bridge.yaml"
     )
 
-    
+    model_path = str(Path(delivery_description).parent.resolve())
+    model_path += pathsep + os.path.join(get_package_share_directory("delivery_description"), 'models')
+
+    gazebo_resource_path = SetEnvironmentVariable(
+        "GZ_SIM_RESOURCE_PATH",
+        model_path
+        )
+
+    ros_distro = os.environ["ROS_DISTRO"]
+    is_ignition = "True" if ros_distro == "humble" else "False"
+
+    robot_description = ParameterValue(Command([
+            "xacro ",
+            LaunchConfiguration("model"),
+            " is_ignition:=",
+            is_ignition
+        ]),
+        value_type=str
+    )
 
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
@@ -63,10 +67,9 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
-                launch_arguments=[
-                    ("gz_args", [" -v 4", " -r", " empty.sdf"]
-                    )
-                ]
+                launch_arguments={
+                    "gz_args": PythonExpression(["'", world_path, " -v 4 -r'"])
+                }.items()
              )
 
     gz_spawn_entity = Node(
@@ -85,14 +88,12 @@ def generate_launch_description():
         
     )
 
-    
-    
     return LaunchDescription([
         model_arg,
+        world_name_arg,
         gazebo_resource_path,
         robot_state_publisher_node,
         gazebo,
         gz_spawn_entity,
-        gz_ros2_bridge,
-        
+        gz_ros2_bridge
     ])
