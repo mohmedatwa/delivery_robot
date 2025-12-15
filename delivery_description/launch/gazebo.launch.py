e7e9a2f25c6f5f738e40fb21dc55013a0e9db617
Mohamed Atwa <mohamedatwa50@hotmail.com>
2025-12-15 09:43:05 +0200

    Modify gazebo launch file

--- delivery_description/launch/gazebo.launch.py
+++ delivery_description/launch/gazebo.launch.py
@@ -1,11 +1,10 @@
 import os
-from os import pathsep
 from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, PythonExpression
+from launch.substitutions import Command, LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -15,39 +14,28 @@ from launch_ros.parameter_descriptions import ParameterValue
 def generate_launch_description():
     delivery_description = get_package_share_directory("delivery_description")
 
-    model_arg = DeclareLaunchArgument(
-        name="model", default_value=os.path.join(
-                delivery_description, "urdf", "delivery.urdf.xacro"
-            ),
-        description="Absolute path to robot urdf file"
-    )
-
-    world_name_arg = DeclareLaunchArgument(name="world_name", default_value="empty")
-
-    world_path = PathJoinSubstitution([
-            delivery_description,
-            "worlds",
-            PythonExpression(expression=["'", LaunchConfiguration("world_name"), "'", " + '.world'"])
-        ]
-    )
-
     bridge_config = os.path.join(
     get_package_share_directory("delivery_description"),
     "config",
     "ros_gz_bridge.yaml"
     )
 
-    model_path = str(Path(delivery_description).parent.resolve())
-    model_path += pathsep + os.path.join(get_package_share_directory("delivery_description"), 'models')
+    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
+                                        delivery_description, "urdf", "delivery.urdf.xacro"
+                                        ),
+                                      description="Absolute path to robot urdf file"
+    )
 
     gazebo_resource_path = SetEnvironmentVariable(
-        "GZ_SIM_RESOURCE_PATH",
-        model_path
+        name="GZ_SIM_RESOURCE_PATH",
+        value=[
+            str(Path(delivery_description).parent.resolve())
+            ]
         )
-
+    
     ros_distro = os.environ["ROS_DISTRO"]
     is_ignition = "True" if ros_distro == "humble" else "False"
-
+    
     robot_description = ParameterValue(Command([
             "xacro ",
             LaunchConfiguration("model"),
@@ -67,9 +55,10 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
-                launch_arguments={
-                    "gz_args": PythonExpression(["'", world_path, " -v 4 -r'"])
-                }.items()
+                launch_arguments=[
+                    ("gz_args", [" -v 4", " -r", " empty.sdf"]
+                    )
+                ]
              )
 
     gz_spawn_entity = Node(
@@ -90,10 +79,9 @@ def generate_launch_description():
 
     return LaunchDescription([
         model_arg,
-        world_name_arg,
         gazebo_resource_path,
         robot_state_publisher_node,
         gazebo,
         gz_spawn_entity,
-        gz_ros2_bridge
+        gz_ros2_bridge,
     ])
