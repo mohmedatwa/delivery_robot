c18e99e813aa442fad960ff7a60f365bae44b58c
Mohamed Atwa <mohamedatwa50@hotmail.com>
2026-02-21 00:32:22 +0200

    change launch file

--- delivery_navigation/launch/delivary_nav.launch.py
+++ delivery_navigation/launch/delivary_nav.launch.py
@@ -1,145 +1,52 @@
-#!/usr/bin/env python3
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription,DeclareLaunchArgument
-import os 
-from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-
+import os
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    map_name = LaunchConfiguration("map_name")
-    use_sim_time = LaunchConfiguration("use_sim_time")
-    nav_params = LaunchConfiguration("nav_params")
-    delivery_navigation_dir = get_package_share_directory("delivery_navigation")
-    lifecycle_nodes = [
-        "map_server","amcl",
-        "controller_server", "planner_server", 
-        "smoother_server", "bt_navigator", 
-        "behavior_server"
-        ]
-    map_name_arg = DeclareLaunchArgument(
-        "map_name",
-        default_value="small_warehouse.yaml"
+    delivery_nav_dir = get_package_share_directory('delivery_navigation')
+    nav2_bringup_launch_file_dir = os.path.join(
+        get_package_share_directory('nav2_bringup'), 'launch', 'bringup_launch.py'
     )
-
-    use_sim_time_arg = DeclareLaunchArgument(
-        "use_sim_time",
-        default_value="true"
-    )
-    nav_params_arg = DeclareLaunchArgument(
-        "nav_params",
-        default_value=os.path.join(
-            delivery_navigation_dir,
-            "config",
-            "nav2_params.yaml"
-        ),
-        description="Full path to navigatian yaml file paramter to load"
-    )
-
-    map_path = PathJoinSubstitution([
-        delivery_navigation_dir,
-        "maps",
-        map_name
-        
-    ])
-    
-    nav2_controller_server = Node(package="nav2_controller",
-        executable="controller_server",
-        output="screen",
-        parameters=[
-            nav_params,
-            {"use_sim_time": use_sim_time}
-        ],)
-    
-    nav2_planner_server = Node(
-        package="nav2_planner",
-        executable="planner_server",
-        name="planner_server",
-        output="screen",
-        parameters=[
-            nav_params,
-            {"use_sim_time": use_sim_time}
-        ],
-    )
-
-    nav2_behaviors = Node(
-        package="nav2_behaviors",
-        executable="behavior_server",
-        name="behavior_server",
-        output="screen",
-        parameters=[
-            nav_params,
-            {"use_sim_time": use_sim_time}
-        ],
-    )
-    
-    nav2_bt_navigator = Node(
-        package="nav2_bt_navigator",
-        executable="bt_navigator",
-        name="bt_navigator",
-        output="screen",
-        parameters=[
-            nav_params,
-            {"use_sim_time": use_sim_time}
-        ],
-    )
-
-    nav2_smoother_server = Node(
-        package="nav2_smoother",
-        executable="smoother_server",
-        name="smoother_server",
-        output="screen",
-        parameters=[
-            nav_params,
-            {"use_sim_time": use_sim_time}
-        ],
-    )
-
-
-
-    nav2_map_server = Node(
-        package="nav2_map_server",
-        executable="map_server",
-        name="map_server",
-        output="screen",
-        parameters=[
-            {"yaml_filename": map_path},
-            {"use_sim_time": use_sim_time}
-        ],
-    )
-
-    nav2_amcl = Node(
-        package="nav2_amcl",
-        executable="amcl",
-        name="amcl",
-        output="screen",
-        emulate_tty=True,
-        parameters=[
-            nav_params,
-            {"use_sim_time": use_sim_time},
-        ],
-    )
-
-    nav2_lifecycle_manager = Node(
-        package="nav2_lifecycle_manager",
-        executable="lifecycle_manager",
-        name="lifecycle_manager_navigation",
-        output="screen",
-        parameters=[
-            {"node_names": lifecycle_nodes},
-            {"use_sim_time": use_sim_time},
-            {"autostart": True}
-        ],
-    )
-
-
     rviz_config = os.path.join(
     get_package_share_directory("nav2_bringup"),
     "rviz",
     "nav2_default_view.rviz")
+
+    map_yaml_file = LaunchConfiguration('map')
+    params_file = LaunchConfiguration('params_file')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        'map',
+        default_value=os.path.join(delivery_nav_dir, 'maps', 'small_house.yaml'),
+        description='Full path to map yaml file to load',
+    )
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(delivery_nav_dir, 'config', 'nav2_params.yaml'),
+        description='Full path to Nav2 parameters file to load',
+    )
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time', 
+        default_value='false', 
+        description='Use simulation (Gazebo) clock if true'
+    )
+
+    nav2_bringup_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([nav2_bringup_launch_file_dir]),
+        launch_arguments={
+            'map': map_yaml_file,
+            'params_file': params_file,
+            'use_sim_time': use_sim_time,
+        }.items(),
+    )
 
     rviz_node = Node(
     package="rviz2",
@@ -147,26 +54,15 @@ def generate_launch_description():
     name="rviz2",
     output="screen",
     arguments=["-d", rviz_config],
-    parameters=[{"use_sim_time": True}]
-    )     
-        
-    
-    return LaunchDescription([
-        map_name_arg,
-        use_sim_time_arg,
-        nav_params_arg,
-        nav2_map_server,
-        nav2_amcl,
-        nav2_controller_server,
-        nav2_planner_server,
-        nav2_smoother_server,
-        nav2_behaviors,
-        nav2_bt_navigator,
-        nav2_lifecycle_manager,
-        rviz_node,
+    parameters=[{"use_sim_time": use_sim_time}]
+    )    
 
-    ])
-
-    
-
-
+    return LaunchDescription(
+        [
+            declare_map_yaml_cmd,
+            declare_params_file_cmd,
+            declare_use_sim_time_cmd,
+            nav2_bringup_launch,
+            rviz_node
+        ]
+    )
