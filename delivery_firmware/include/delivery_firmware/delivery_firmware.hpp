3d652300df91f651e13cb63629d5ae78148c3cfc
Mohamed Atwa <mohamedatwa50@hotmail.com>
2026-02-25 09:00:09 +0200

    update firmware

---
+++ delivery_firmware/include/delivery_firmware/delivery_firmware.hpp
@@ -0,0 +1,43 @@
+#ifndef DELIVERY_FIRMWARE_HPP_
+#define DELIVERY_FIRMWARE_HPP_
+
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+#include <vector>
+#include <string>
+
+namespace delivery_firmware
+{
+
+class DeliveryFirmware : public hardware_interface::SystemInterface
+{
+public:
+  hardware_interface::CallbackReturn on_init(
+    const hardware_interface::HardwareInfo & info) override;
+
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+  hardware_interface::return_type read(
+    const rclcpp::Time & time,
+    const rclcpp::Duration & period) override;
+
+  hardware_interface::return_type write(
+    const rclcpp::Time & time,
+    const rclcpp::Duration & period) override;
+
+private:
+  std::vector<double> hw_positions_;
+  std::vector<double> hw_velocities_;
+  std::vector<double> hw_commands_;
+
+  std::string device_;
+  int baud_rate_;
+};
+
+}
+
+#endif
