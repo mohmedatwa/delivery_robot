5dc28148fa0333c7ac390c5b7d736b6db0337bdd
Mohamed Atwa <mohamedatwa50@hotmail.com>
2026-04-21 10:54:38 +0200

    add delay time for navigation

--- delivery_bringup/launch/simulated_delivery_robot.launch.py
+++ delivery_bringup/launch/simulated_delivery_robot.launch.py
@@ -2,10 +2,11 @@
 
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction
 from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
     rviz_config = os.path.join(
@@ -19,15 +20,17 @@ def generate_launch_description():
     )
     use_sim_time = LaunchConfiguration('use_sim_time')
     gazebo = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(
         os.path.join(
             get_package_share_directory("delivery_description"),
             "launch",
             "gazebo.launch.py"
-        ),
-        launch_arguments={
-            "use_sim_time": use_sim_time
-        }.items()
-    )
+        )
+    ),
+    launch_arguments={
+        "use_sim_time": use_sim_time
+    }.items()
+)
     
     controller = IncludeLaunchDescription(
         os.path.join(
@@ -80,13 +83,17 @@ def generate_launch_description():
             "use_sim_time": use_sim_time
         }.items()
     )
-
+    navigation_delayed = TimerAction(
+    period=5.0,  # delay in seconds
+    actions=[navigation]
+)
     rviz_node = Node(
     package="rviz2",
     executable="rviz2",
     name="rviz2",
     output="screen",
     arguments=["-d", rviz_config],
+    parameters=[{"use_sim_time": use_sim_time}]
     )   
 
     return LaunchDescription([
@@ -94,7 +101,7 @@ def generate_launch_description():
         gazebo,
         controller,
         joystick,
-        navigation,
+        navigation_delayed,
         utils,
         rviz_node,
         # localization,
